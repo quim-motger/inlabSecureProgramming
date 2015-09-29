20df50870243c87b9903da0951fc3050f82d5f00
jaumem <jaumem@JAKARTA.tolkien.fib.upc.es>
2015-09-29 17:01:56 +0200

    Faltaven 2 coses

---
+++ src/main/java/edu/upc/escert/curs/LoginServlet.java
@@ -0,0 +1,42 @@
+package edu.upc.escert.curs;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.upc.escert.curs.repositori.IRepositoriUsuaris;
+import edu.upc.escert.curs.repositori.RepositoriFactory;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+	IRepositoriUsuaris repositoriUsuaris=RepositoriFactory.getRepositoriUsuaris();
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/login.jsp").forward(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+
+		if (repositoriUsuaris.autenticar(username,password)) {
+			request.getSession().setAttribute("username", username);
+			response.sendRedirect("comentaris");
+		} else {
+			request.setAttribute("login_error",1);
+			doGet(request,response);
+		}
+	}
+
+}
